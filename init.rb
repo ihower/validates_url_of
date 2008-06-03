63a3432e1ddc8d8ede7c34e0a9715fca038ea10f
Wen-Tien Chang <ihower@gmail.com>
2008-06-04 03:56:57 +0800

    first import

---
+++ init.rb
@@ -0,0 +1,5 @@
+require 'validates_url_of'
+
+class ActiveRecord::Base
+  include Handlino::ValidatesUrlOf
+end
---
+++ lib/validates_url_of.rb
@@ -0,0 +1,49 @@
+module Handlino
+  module ValidatesUrlOf
+  
+    def self.included(base)
+      base.send(:extend, ClassMethods)
+    end
+    
+    module ClassMethods
+    
+      def validates_url_of( attr_name, options = {} )
+        options[:message] = (options[:message]) ? options[:message] : 'is not valid or not responding'
+        
+        define_method( "try_fixing_#{attr_name}_url") do
+          value = read_attribute(attr_name)
+          return if value.blank?
+          write_attribute( attr_name, "http://#{value}" ) unless ( value.include?('http://') || value.include?('https://') )
+        end
+        
+        before_validation "try_fixing_#{attr_name}_url".to_sym
+        
+        # modify from http://www.igvita.com/2006/09/07/validating-url-in-ruby-on-rails/ and Validates_URL plugin
+        validates_each attr_name, :allow_blank => true do |r, a, v|
+          if v.to_s =~  /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix # check RegExp
+            if RAILS_ENV == 'production'
+              begin
+                uri = URI.parse(v)
+                if uri.kind_of? URI::HTTP then
+                  r.errors.add(a, options[:message]) unless Net::HTTP.get_response(uri).kind_of? Net::HTTPSuccess
+                else
+                  r.errors.add(a, options[:message])
+                end
+              rescue URI::InvalidURIError
+                r.errors.add(a, options[:message])
+              rescue SocketError, Errno::ECONNREFUSED, Errno::ECONNRESET, Errno::EHOSTUNREACH
+                r.errors.add(a, options[:message])
+              rescue Timeout::Error
+                r.errors.add(a, options[:message])
+              end
+            end
+          else
+            r.errors.add(a, options[:message])
+          end
+        end
+        
+      end
+    end
+
+  end
+end
